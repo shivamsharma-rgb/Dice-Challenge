ce348c03cfb90615c0aad40d3a7d70280d641516
Shivam Sharma <72445648+shivamsharma-rgb@users.noreply.github.com>
2022-10-27 18:35:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let randomNumber1=Math.floor(Math.random()*6)+1;
+let randomNumber2=Math.floor(Math.random()*6)+1;
+
+document.querySelectorAll("img")[0].setAttribute("src","images/dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[1].setAttribute("src","images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🏴‍☠️ Player 1 Wins!";  
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent="Player 2 Wins! 🏴‍☠️";
+}else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").textContent="Draw!";
+}
