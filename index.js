1a99f199ff261199db07a8e2cce583fb9997050f
Shivam Sharma <72445648+shivamsharma-rgb@users.noreply.github.com>
2022-10-27 18:59:37 +0530

    second commit in index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let randomNumber1=Math.floor(Math.random()*6)+1;
 let randomNumber2=Math.floor(Math.random()*6)+1;
 
-document.querySelectorAll("img")[0].setAttribute("dice" + randomNumber1 + ".png");
-document.querySelectorAll("img")[1].setAttribute("dice" + randomNumber2 + ".png");
+document.querySelectorAll("img")[0].setAttribute("src","dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[1].setAttribute("src","dice" + randomNumber2 + ".png");
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML="🏴‍☠️ Player 1 Wins!";  
